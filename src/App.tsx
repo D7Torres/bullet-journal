ef9b670c95b4f8f8e2147e35aa13462a54e2abad
David Torres <david7torres@gmail.com>
2025-01-31 09:38:44 +0100

    chore: add aliases to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Journals } from './pages/Journals/Journals'
+import { Journals } from '@pages/Journals/Journals'
 
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    },
   },
 })

8e2d816eeede03fb48a6f5f9610ea479bbbef756
David Torres <david7torres@gmail.com>
2025-02-04 20:58:09 +0100

    feat: add links to weeks in the journal page

--- src/pages/Journal/Journal.tsx
+++ src/pages/Journal/Journal.tsx
@@ -1,11 +1,28 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import { firstDayOfWeek, lastDayOfWeek } from '@utils/dates'
+
+const weekNumbers = Array.from({ length: 53 }, (_, i) => i + 1)
 
 export const Journal = () => {
   const { year } = useParams()
 
+  if (!year) {
+    return null
+  }
+
   return (
     <>
       <h1>Journal {year}</h1>
+      <ul>
+        {weekNumbers.map((weekNumber) => (
+          <li key={weekNumber}>
+            <Link to={`/journal/${year}/week/${weekNumber}`}>
+              {firstDayOfWeek(+year, weekNumber)} -{' '}
+              {lastDayOfWeek(+year, weekNumber)}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
